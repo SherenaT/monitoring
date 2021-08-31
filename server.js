ba28085054913b4c805042aaa9b02af02e64c5b6
Sherena Tu <s.tu314@yahoo.com>
2021-08-31 10:39:53 -0700

    implemented rollbar

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require("path"); //variable we use to tell the deployment were going to use where we are at
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: "4233ac1ab91c4319b588ebf63e0c7140",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+  rollbar.info("html file served successfully.");
 });
 
 const port = process.env.PORT || 4545;
