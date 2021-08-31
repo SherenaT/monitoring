92094b13d725a2949688eabbbfbf03c107d37c4b
Sherena Tu <s.tu314@yahoo.com>
2021-08-31 11:35:19 -0700

    new html

--- server.js
+++ server.js
@@ -11,23 +11,47 @@ const rollbar = new Rollbar({
 const students = [];
 const app = express();
 app.use(express.json());
+app.use("/style", express.static("./public/styles.css")); //if anybody tries to load a style page (by going to /style) it will load CSS page by going to punlic style.css
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
   rollbar.info("html file served successfully.");
 });
 
+// app.post("/api/student", (req, res) => {
+//   let { name } = req.body;
+//   name = name.trim(); //trim whitespace
+
+//   students.push(name);
+
+//   rollbar.log("Student add successfully", {
+//     author: "Sherena",
+//     type: "manual entry",
+//   });
+//   res.status(200).send(students);
+// });
+
 app.post("/api/student", (req, res) => {
   let { name } = req.body;
-  name = name.trim(); //trim whitespace
+  name = name.trim();
 
-  students.push(name);
+  const index = students.findIndex((studentName) => studentName === name); //find index to loop over the name and see if students name already exist
 
-  rollbar.log("Student add successfully", {
-    author: "Sherena",
-    type: "manual entry",
-  });
-  res.status(200).send(students);
+  if (index === -1 && name !== "") {
+    //if students name isnt in there are an empty string
+    students.push(name);
+    rollbar.log("Student add successfully", {
+      author: "Sherena",
+      type: "manual entry",
+    });
+    res.status(200).send(students);
+  } else if (name === "") {
+    rollbar.error("No given name");
+    res.status(400).send("must provide a name.");
+  } else {
+    rollbar.error("stuent already exists");
+    res.status(400).send("that student already exists");
+  }
 });
 
 const port = process.env.PORT || 4545;
